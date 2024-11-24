8c45dbd7f64d15265a7c7f1e534a8c928122891e
y-syo <mmoussou@student.42angouleme.fr>
2024-11-24 15:28:10 +0100

    「✨」 feat(0/ex01): added signal handling

--- 0/ex01/main.cpp
+++ 0/ex01/main.cpp
@@ -6,14 +6,16 @@
 /*   By: mmoussou <mmoussou@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 10:03:20 by mmoussou          #+#    #+#             */
-/*   Updated: 2024/09/18 11:48:34 by mmoussou         ###   ########.fr       */
+/*   Updated: 2024/11/24 15:27:33 by mmoussou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
+#include <csignal>
 
 int	main(void)
 {
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
 	PhoneBook phonebook;
-	//Contact contact1("John", "Shakoh", "caca", "192.18.0.24", "kakoh");
 }

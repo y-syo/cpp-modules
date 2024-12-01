db59b19ebbfc5155b3f9f1cb37c574510feefb03
y-syo <mmoussou@student.42angouleme.fr>
2024-12-01 21:33:09 +0100

    「✨」 feat(flake.nix): cool flake :D

--- flake.nix
+++ flake.nix
@@ -2,22 +2,38 @@
   description = "a development environment for the c++ modules";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs = {
+      url = "nixpkgs/nixos-24.11";
+      flake = false;
+	};
+
+    pogit = {
+      url = "github:y-syo/pogit";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, flake-utils, nixpkgs, ... }:
-  flake-utils.lib.eachDefaultSystem(system:
-  let
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    devShells.default = pkgs.mkShell {
-	  packages = with pkgs; [ clang gcc norminette valgrind gdb ];
-	  hardeningDisable = [ "all" ];
-	  buildInputs = with pkgs; [];
-	  shellHook = ''
-		echo -e "\x1B[0;33mentering c++ environment...\x1B[0m"
-	  '';
-	};
-  });
+  outputs = inputs@{ nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+	{
+    devShells = forEachSupportedSystem ({ pkgs }: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          clang gcc norminette valgrind gdb gnumake cmake clang-tools git
+		  inputs.pogit.packages.${pkgs.system}.default
+        ];
+        hardeningDisable = [ "all" ];
+        shellHook = ''
+          export CC=gcc
+          export CXX=g++
+          echo -e "\x1B[0;33mentering c++ development environment...\x1B[0m"
+        '';
+      };
+    });
+  };
 }
